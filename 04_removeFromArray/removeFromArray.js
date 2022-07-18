4d4658cd7d13e1b88822b3041f35ec6cfddb1152
mishalp <mishalclever@gmail.com>
2022-07-18 20:19:04 +0530

    04_removeFromArray passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr , ...arg) {
+ 
+    for(i=0;i<arr.length;i++){
+        for(j of arg){
+            if(arr[i] === j){
+                arr.splice(i,1)
+                i--;
+            }
+
+        }
+       
+       
+    }
+    return arr
 
 };
 
