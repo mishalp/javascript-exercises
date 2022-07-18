e6600304df373b1c6aad480931369b60c7c5245f
mishalp <mishalclever@gmail.com>
2022-07-18 20:37:03 +0530

    05_sumAll passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1 , num2) {
+ 
+    let sum = 0;
+    if(!(Number.isInteger(num1)) || !(Number.isInteger(num2)) ){
+        return "ERROR"
+    }
+
+    if(num1<0 || num2 <0){
+        return "ERROR"
+    }
+
+    if(num2 > num1){
+        for(i=num1;i<=num2;i++){
+            sum = sum + i 
+        }
+        return sum
+    }else{
+        for(i=num2;i<=num1;i++){
+            sum = sum + i;
+        }
+        return sum
+    }
+
 
 };
 
