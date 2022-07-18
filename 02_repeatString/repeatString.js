8610ebf54a95743e4a6549361b8f263477218d8a
mishalp <mishalclever@gmail.com>
2022-07-18 18:18:39 +0530

    02_repeatString passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if(num<0){
+        return "ERROR"
+    }
+ let word = ""
+    for(i=0;i<num;i++){
+        word = word+string;
+    }
+    return word
 
 };
 
