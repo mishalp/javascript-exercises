61c82cc043c84a5a437275895d56a3a56cfed2ba
mishalp <mishalclever@gmail.com>
2022-07-19 17:58:12 +0530

    08_calculator passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,48 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1+num2
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+	return num1-num2
+};
+
+const sum = function(arr) {
+  let value=0
+  for(let i=0;i<arr.length;i++){
+    value = value + arr[i]
+  }
+  return value
 	
 };
 
-const sum = function() {
+const multiply = function(arr) {
+
+  let value=1
+  for(let i=0;i<arr.length;i++){
+    value = value * arr[i]
+  }
+  return value
+
+};
+
+const power = function(num1, num2) {
+
+  return num1**num2
 	
 };
 
-const multiply = function() {
+const factorial = function(num) {
 
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+  if(num == 0 || num ==1){
+    return 1
+  }
+  while(num){
+    if(num == 2){
+      return 2
+    }
+    return num*factorial(num-1)
+  }
 	
 };
 
