4746fd3c7fde3efd32b4a3ea94bcb534529864b6
mishalp <mishalclever@gmail.com>
2022-07-19 18:44:38 +0530

    09_palindroms passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,31 @@
-const palindromes = function () {
+const palindromes = function (words) {
+
+    words = words.toLowerCase()
+    let rev =""
+
+    function isLetter(char) {
+        return (/[a-zA-Z]/).test(char)
+      }
+      for(i=words.length-1;i>=0;i--){
+        if(isLetter(words[i])){
+            rev= rev + words[i]
+
+
+        }else{
+            
+            words = words.replace(words[i],"")
+            
+        }
+      }
+    
+      if(words==rev){
+        return true
+      }else{
+        return false
+      }
 
 };
+palindromes('racecar!')
 
 // Do not edit below this line
 module.exports = palindromes;
