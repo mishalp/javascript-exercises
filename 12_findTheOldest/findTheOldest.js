00a111cd465184a0d6f2e51b106b0a31a4c5c09a
mishalp <mishalclever@gmail.com>
2022-07-19 20:17:12 +0530

    12_findTheOldest passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrOfObj) {
+    let pos =0 
+    let temp = 0
+    if(arrOfObj[0].yearOfDeath){
+        temp = arrOfObj[0].yearOfDeath - arrOfObj[0].yearOfBirth
 
+    }else{
+        const d = new Date();
+         
+        temp = d.getFullYear() - arrOfObj[0].yearOfBirth
+    }
+    for(let i=1;i<arrOfObj.length;i++){
+        if(temp < arrOfObj[i].yearOfDeath - arrOfObj[i].yearOfBirth){
+            temp = arrOfObj[i].yearOfDeath - arrOfObj[i].yearOfBirth
+            pos = i
+        }
+    }
+    return arrOfObj[pos]
 };
 
 // Do not edit below this line
