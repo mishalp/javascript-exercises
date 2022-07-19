e28fc12be02637234a4ee9e13b40e913ec4bef94
mishalp <mishalclever@gmail.com>
2022-07-19 18:58:09 +0530

    10_fibinacci passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num)
+
+    let Phi = (1+Math.sqrt(5))/2
+    let phi = (1-Math.sqrt(5))/2
+
+    if(num>0){
+     val = ((Phi**num)-(phi**num))/Math.sqrt(5)
+     return parseInt(val)
+    }else{
+        return "OOPS"
+    }
 
 };
 
